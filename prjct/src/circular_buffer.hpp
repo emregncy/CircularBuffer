3c053668901ca7c69ab894aed46c0e0b172e6cfc
emregncy <emregncy56@gmail.com>
2024-09-25 22:47:33 +0300

    Circular buffer with STL

---
+++ prjct/src/circular_buffer.hpp
@@ -0,0 +1,90 @@
+#ifndef _circular_buffer_hpp
+#define _circular_buffer_hpp
+
+#include <iostream>
+#include <array>
+#include <stdexcept>
+
+using namespace std;
+template <typename T, int cap, typename Container = array<T, cap>>
+class CircularBuffer {
+
+    public:
+        CircularBuffer(): _head(0), _tail(0), _size(0), _current(0), _capacity(cap) {}
+
+
+        T& head() {
+            return c.at(_head);
+        }
+        T& tail() {
+            return c.at(_tail);
+        }
+
+        T const& head() const {
+            return c.at(_head);
+        }
+        T const& tail() const {
+            return c.at(_tail);
+        }
+
+        void push_back(T val) noexcept {
+            if (_current >= _capacity) {
+                _current = 0;
+
+            }
+            c.at(_current++) = val;
+            
+            _tail = _current - 1;
+        
+            if (_size++ >= _capacity) {
+                _size = _capacity;
+                _head++;
+                if (_head >= _capacity) {
+                    _head = 0;
+                }
+            }        
+        }
+        void place_back(T val) {
+            if (full()) {
+                throw overflow_error("place_back(): full buffer");
+            }
+        }
+
+        void pop() {
+            if (empty()) {
+                throw underflow_error("pop(): empty buffer");
+            }
+
+               _head++;
+                if (_head >= _capacity) {
+                    _head = 0;
+                }
+
+                _size--;
+        }
+
+        size_t size() const noexcept {
+            return _size;
+        }
+        size_t capacity() const noexcept {
+            return _capacity;
+        }
+
+        bool empty() const noexcept {
+            return _size <= 0;
+        }
+        bool full() const noexcept {
+            return _size >= _capacity;
+        }
+
+    private :
+        Container c;
+
+        int _head;
+        int _tail;
+        int _size;
+        int _capacity;
+        int _current;
+    };
+
+#endif
